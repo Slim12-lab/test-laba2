deda51db6db74852f3aecb32c8078ef3fad03acf
Savelii Nikonov <nikonov@cs.petrsu.ru>
2023-12-05 18:15:23 +0300

    fix bugs

--- main.py
+++ main.py
@@ -83,40 +83,39 @@ if __name__ == '__main__':  # pragma: no cover
             "Вы стали миллиардером!"
             quit()
         command = input()
-        match command:
-            case "stat":
-                print("Баланс: " + str(game.player.balance))
-                print("Доход: " + str(game.player.income) + "$")
-                print("Недвижимость: ")
-                for building in game.buildings:
-                    building_str = building.name
-                    while len(building_str) < 30 - len(str(building.count)):
-                        building_str += " "
-                    print(building_str + str(building.count))
+        if command == 'stat':
+            print("Баланс: " + str(game.player.balance))
+            print("Доход: " + str(game.player.income) + "$")
+            print("Недвижимость: ")
+            for building in game.buildings:
+                building_str = building.name
+                while len(building_str) < 30 - len(str(building.count)):
+                    building_str += " "
+                print(building_str + str(building.count))
 
-            case "store":
-                print("Магазин\n")
-                for building in game.buildings:
-                    building_str = building.name
-                    while len(building_str) < 30 - len(str(building.price)):
-                        building_str += " "
-                    print(building_str + str(building.price) + "$")
+        elif command =="store":
+            print("Магазин\n")
+            for building in game.buildings:
+                building_str = building.name
+                while len(building_str) < 30 - len(str(building.price)):
+                    building_str += " "
+                print(building_str + str(building.price) + "$")
 
-                while True:
-                    print("\nВведите название недвижимости, чтобы купить её, или back, чтобы выйти из меню покупки:\n")
-                    temp = input()
-                    if (temp == 'back'):
-                        print("Вы отказались от покупки!")
-                        break
-                    print(game.buy(temp))
-            case "work":
-                print("+" + str(game.player.income))
-                print("Баланс: " + str(game.player.plus_balance()))
-            case "help":
-                print( "Команды:\n1. Баланс, доход и недвижимость: balance\n2. Магазин: store\n3. Работать: work\n4. Команды: help\n5. Выйти: quit")
-            case "quit":
-                quit()
-            case _:
-                print("Вы ввели некорректную команду! Попробуйте ещё раз.")
+            while True:
+                print("\nВведите название недвижимости, чтобы купить её, или back, чтобы выйти из меню покупки:")
+                temp = input()
+                if (temp == 'back'):
+                    print("Вы отказались от покупки!")
+                    break
+                print(game.buy(temp))
+        elif command == "work":
+            print("+" + str(game.player.income) + "$")
+            print("Баланс: " + str(game.player.plus_balance()) + "$")
+        elif command == "help":
+            print( "Команды:\n1. Баланс, доход и недвижимость: stat\n2. Магазин: store\n3. Работать: work\n4. Команды: help\n5. Выйти: quit")
+        elif command == "quit":
+            quit()
+        else:
+            print("Вы ввели некорректную команду! Попробуйте ещё раз.")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
